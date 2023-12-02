276b3e10d87baf2e36f6145871641ee0c13f445f
Sahidutissera <130826490+Sahidutissera@users.noreply.github.com>
2023-12-02 09:11:19 +0100

    Add files via upload

---
+++ part 2 es 7010.ps1
@@ -0,0 +1,6 @@
+﻿$sorgente = Read-Host "inserire la sorgente"
+$destinazione =Read-Host "inserire la destinazione"
+if(Test-Path $sorgente) { if(test-path $destinazione) { Move-Item $sorgente\*.* $destinazione} else {echo alemeno uno delle due non esiste}
+else { echo alemeno uno delle due non esiste}
+
+
